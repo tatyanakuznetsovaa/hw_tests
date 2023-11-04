85698746ba7b3a76f91945ae49ef7d7d9f552b6a
Татьяна <tatyana@MacBook-Air.local>
2023-11-04 21:36:43 +0300

    black

--- issue_03.py
+++ issue_03.py
@@ -5,15 +5,15 @@ import unittest
 
 class TestFitTransform(unittest.TestCase):
     def test_fit_transform_default_case(self):
-        actual = fit_transform(['Moscow', 'New York', 'Moscow', 'London'])
+        actual = fit_transform(["Moscow", "New York", "Moscow", "London"])
         expected = [
-            ('Moscow', [0, 0, 1]),
-            ('New York', [0, 1, 0]),
-            ('Moscow', [0, 0, 1]),
-            ('London', [1, 0, 0]),
+            ("Moscow", [0, 0, 1]),
+            ("New York", [0, 1, 0]),
+            ("Moscow", [0, 0, 1]),
+            ("London", [1, 0, 0]),
         ]
         self.assertEqual(actual, expected)
-    
+
     def test_fit_transform_empty_case(self):
         actual = fit_transform([])
         expected = []
@@ -29,13 +29,10 @@ class TestFitTransform(unittest.TestCase):
 
     def test_fit_transform_noniter_args_good_seq(self):
         actual = fit_transform("Moscow", 2)
-        expected = [
-            ('Moscow', [0, 1]),
-            (2, [1, 0])
-        ]
+        expected = [("Moscow", [0, 1]), (2, [1, 0])]
 
         self.assertListEqual(actual, expected)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
