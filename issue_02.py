f72d41a30ab11f31dc2f7130f48bed1f452af4a6
Татьяна <tatyana@MacBook-Air.local>
2023-11-04 18:56:34 +0300

    added hw issue2

---
+++ issue_02.py
@@ -0,0 +1,13 @@
+from morse import decode
+import pytest
+
+@pytest.mark.parametrize(
+    "source_string,result", 
+    [
+        ('... --- ...', 'SOS'), 
+        ('.... . .-.. .-.. ---', 'HELLO'),
+        ('.- ...- .. - ---', 'AVITO')
+    ], 
+)
+def test_decode(source_string, result): 
+    assert decode(source_string) == result
